96022f4711c128bb3027d325e6ab9d45c04836d3
Alex Khanin <akh.alex.khan@gmail.com>
2020-12-18 21:51:01 +0200

    Make read timeout is bigger

--- connector/c2cCon.go
+++ connector/c2cCon.go
@@ -108,7 +108,7 @@ func (c *Connection) Write(to string, command uint16, data []byte) error {
 }
 
 func (c *Connection) Read() (from string, command uint16, data []byte, err error) {
-	c.conn.SetReadDeadline(time.Now().Add(c.readTimeout))
+	c.conn.SetReadDeadline(time.Now().Add(10 * c.readTimeout))
 	reader := bufio.NewReader(c.conn)
 	receiveBuff, err := c.p.ReadPacketHeader(reader)
 	if err != nil {
@@ -120,7 +120,7 @@ func (c *Connection) Read() (from string, command uint16, data []byte, err error
 	}
 	if restSize > 0 {
 		resp := make([]byte, restSize)
-		c.conn.SetReadDeadline(time.Now().Add(time.Duration(restSize) * 10 * time.Millisecond))
+		c.conn.SetReadDeadline(time.Now().Add(time.Duration(restSize) * c.readTimeout))
 		_, err := io.ReadFull(reader, resp)
 		if err != nil {
 			return "", 0, nil, err
